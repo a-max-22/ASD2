ae3fb0054cf0329992b83bd0476d3625776e6496
a-max-22 <108484061+a-max-22@users.noreply.github.com>
2022-11-14 13:34:53 +0300

    balanced BST generation and check

---
+++ BalancedBST.py
@@ -0,0 +1,60 @@
+class BSTNode:
+	
+    def __init__(self, key, parent):
+        self.NodeKey = key # ключ узла
+        self.Parent = parent # родитель или None для корня
+        self.LeftChild = None # левый потомок
+        self.RightChild = None # правый потомок
+        self.Level = 0 # уровень узла
+
+
+class BalancedBST:
+		
+    def __init__(self):
+    	self.Root = None # корень дерева
+    
+    def MakeNode(self, a, startIndex, endIndex, depth):
+        #print(startIndex, endIndex)
+        if endIndex < startIndex:
+            return None
+        if endIndex - startIndex == 0:
+            return BSTNode(key = a[startIndex], parent = None)
+        parentIndex = startIndex + (endIndex - startIndex) // 2
+        parent = BSTNode(key = a[parentIndex], parent = None)
+        leftChild = self.MakeNode(a, startIndex, parentIndex - 1, depth + 1)
+        rightChild = self.MakeNode(a, parentIndex + 1, endIndex, depth + 1)
+        parent.LeftChild = leftChild
+        parent.RightChild = rightChild
+        parent.Level = depth
+        if leftChild is not None:
+            leftChild.Parent = parent
+        if rightChild is not None:
+            rightChild.Parent = parent
+        
+        return parent
+
+    def GenerateTree(self, a):
+        sortedArr = a.copy()
+        sortedArr.sort()
+        node = self.MakeNode(sortedArr, 0, len(sortedArr)-1, 0)
+        self.Root = node 
+
+    def CheckBalanceAndDepth(self, root_node):
+        if root_node is None:
+            return True, 0
+
+        isLeftSubtreeBalanced, leftSubTreeDepth = self.CheckBalanceAndDepth(root_node.LeftChild)
+        if not isLeftSubtreeBalanced: return False, leftSubTreeDepth + 1
+
+        isRightSubtreeBalanced, rightSubTreeDepth = self.CheckBalanceAndDepth(root_node.RightChild)
+        if not isRightSubtreeBalanced: return False, max(rightSubTreeDepth, leftSubTreeDepth)+1 
+        
+        isBalanced = True
+        if abs(leftSubTreeDepth - rightSubTreeDepth) > 1:
+            isBalanced = False
+        return isBalanced, max(rightSubTreeDepth, leftSubTreeDepth) + 1 
+
+
+    def IsBalanced(self, root_node):
+        isBalanced, _ = self.CheckBalanceAndDepth(root_node)
+        return isBalanced
---
+++ test_BalancedBST.py
@@ -0,0 +1,190 @@
+from BalancedBST import BalancedBST, BSTNode
+import unittest
+
+
+def visitBstNode(node):
+    if node is None: return []
+    return visitBstNode(node.LeftChild) + [node] + visitBstNode(node.RightChild) 
+
+def getBstNodesAscending(bst):
+    nodesLitsAscending = visitBstNode(bst.Root)
+    return nodesLitsAscending
+
+def printNode(node):
+    print(node.NodeKey, node.LeftChild.NodeKey if node.LeftChild is not None else None , node.RightChild.NodeKey if node.RightChild is not None else None)
+
+def verifyBST(bst): 
+    nodes = getBstNodesAscending(bst)
+    if nodes == [] and bst.Root is None: return True
+
+    nodesKeys = [x.NodeKey for x in nodes]
+    if not sorted(nodesKeys): return False
+    rootAlreadyWasInNodesList = False
+    for node in nodes:
+        if node.LeftChild is not None and not( node.LeftChild.NodeKey < node.NodeKey):
+            print("l")
+            return False
+        if node.RightChild is not None and not( node.RightChild.NodeKey > node.NodeKey): 
+            print("r")
+            return False
+
+        if node.Parent is None and rootAlreadyWasInNodesList: 
+            print("Two roots")
+            return False
+            
+        if node.Parent is None: rootAlreadyWasInNodesList = True
+        parent = node.Parent
+        if parent is not None:
+            if parent.LeftChild != node and parent.RightChild != node: 
+                print("Error, node parent has wrong children: parent, node")
+                printNode(parent)
+                printNode(node)
+                return False
+    return True
+
+def makeParentAndAllChildren(parentKey):
+    root = BSTNode(key = parentKey, parent = None)
+    left = BSTNode(key = parentKey - (parentKey // 2), parent = root)
+    right = BSTNode(key = parentKey + (parentKey // 2), parent = root)
+    root.LeftChild = left
+    right.RightChild  =  right 
+    return root  
+
+
+def addLeftChild(parentNode: BSTNode):
+    parentKey = parentNode.NodeKey
+    left = BSTNode(key = parentKey - (parentKey // 2), parent = parentNode)
+    parentNode.LeftChild = left
+    return left
+
+def addRightChild(parentNode: BSTNode):
+    parentKey = parentNode.NodeKey
+    right = BSTNode(key = parentKey + (parentKey // 2), parent = parentNode)
+    parentNode.RightChild = right
+    return right
+
+def makeUnbalancedTreeDepth3():
+    depth = 3
+    root = makeParentAndAllChildren(parentKey = 2**(depth+1))
+    addedNode = addLeftChild(root.LeftChild)
+    addedNode = addLeftChild(addedNode)
+    addedNode = addLeftChild(addedNode)
+    bbst = BalancedBST()
+    bbst.Root = root
+    return bbst 
+ 
+def makeChildNodes(parent: BSTNode, childrenLevelCount):
+    if childrenLevelCount == 1: return 
+
+    childKeyEvaluationDelta = 2 ** (childrenLevelCount-1)
+    leftChildKey = parent.NodeKey - childKeyEvaluationDelta
+    rightChildKey = parent.NodeKey + childKeyEvaluationDelta
+    leftChild = BSTNode(key = leftChildKey, parent = parent)
+    rightChild = BSTNode(key = rightChildKey, parent = parent)
+    parent.LeftChild = leftChild
+    parent.RightChild = rightChild
+    makeChildNodes(leftChild, childrenLevelCount-1)
+    makeChildNodes(rightChild, childrenLevelCount-1)
+
+def makeFullBST(levelCount):
+    basicValue = 2 ** (levelCount+1)
+    rootNode = BSTNode(key = basicValue, parent = None)
+    makeChildNodes(rootNode, levelCount)
+    resultTree = BalancedBST()
+    resultTree.Root = rootNode
+    return resultTree
+
+
+class TestIsBalanced(unittest.TestCase):
+    def testEmptyTree(self):
+        bbst = BalancedBST()
+        self.assertTrue(bbst.IsBalanced(None))
+
+    def testSingleNodeTree(self):
+        root = BSTNode(key = 0 , parent = None)
+        bbst = BalancedBST()
+        self.assertTrue(bbst.IsBalanced(root))
+    
+    def testBalancedTreeDepth1(self):
+        bbst = BalancedBST()
+        bbst.Root = makeParentAndAllChildren(parentKey = 16)
+        self.assertTrue(bbst.IsBalanced(bbst.Root))
+    
+    def testUnbalancedTreeDepth3(self):
+        bbst = makeUnbalancedTreeDepth3()
+        self.assertFalse(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
+
+    def testIsFullTreeBalanced(self):
+        bbst = makeFullBST(levelCount = 4)
+        self.assertTrue(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
+
+    def testUnbalancedTreeBalancedSubtrees(self):
+        rightSubtree = makeFullBST(levelCount = 4)
+        leftSubtree = makeParentAndAllChildren(parentKey = 4)
+        root = BSTNode(key = 9, parent = None)
+        root.LeftChild = leftSubtree
+        leftSubtree.Parent = root
+        root.RightChild = rightSubtree.Root
+        rightSubtree.Root.Parent = root
+        bbst = BalancedBST()
+        bbst.Root = root
+        self.assertFalse(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
+
+
+class TestGenerateBalancedTree(unittest.TestCase):
+
+    def testGenerateTreeFromEmptyList(self):
+        keys = []
+        bbst = BalancedBST()
+        bbst.GenerateTree(keys)
+        self.assertTrue(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
+
+    def testGenerateTreeFromSingleElemList(self):
+        keys = [0]
+        bbst = BalancedBST()
+        bbst.GenerateTree(keys)
+        nodes = getBstNodesAscending(bbst)
+        self.assertEqual(keys, [x.NodeKey for x in nodes])
+        self.assertTrue(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
+
+    def testGenerateTreeFromTwoElemList(self):
+        keys = [0, 1]
+        bbst = BalancedBST()
+        bbst.GenerateTree(keys)
+        nodes = getBstNodesAscending(bbst)
+        self.assertEqual(keys, [x.NodeKey for x in nodes])
+        self.assertTrue(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
+
+    def testGenerateTreeFromThreeElemList(self):
+        keys = [0, 1, 2]
+        bbst = BalancedBST()
+        bbst.GenerateTree(keys)
+        nodes = getBstNodesAscending(bbst)
+        self.assertEqual(keys, [x.NodeKey for x in nodes])
+        self.assertTrue(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
+    
+    def testGenerateTreeFromFourElemList(self):
+        keys = [0, 1, 3, 4]
+        bbst = BalancedBST()
+        bbst.GenerateTree(keys)
+        nodes = getBstNodesAscending(bbst)
+        self.assertEqual(keys, [x.NodeKey for x in nodes])
+        self.assertTrue(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
+
+    def testGenerateTreeFromThreeElemList(self):
+        keys = [16, 12, 20, 10, 14, 18, 22, 9, 11, 13, 15, 17, 19, 21, 23]
+        bbst = BalancedBST()
+        bbst.GenerateTree(keys)
+        nodes = getBstNodesAscending(bbst)
+        keys.sort()
+        self.assertEqual(keys, [x.NodeKey for x in nodes])
+        self.assertTrue(bbst.IsBalanced(bbst.Root))
+        self.assertTrue(verifyBST(bbst))
